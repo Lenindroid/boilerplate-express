16dcec1fbeca89c62a0895a83a872f113bc63ab2
Lenen <leninrojassanchez09@gmail.com>
2023-06-17 15:08:19 -0500

    Root level middleware function

--- myApp.js
+++ myApp.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
-/*console.log('Hello World'); -Interactua con la consola
-app.get("/", (req, res)=>{
-    res.send("Hello Express");
-}); -- Enviaste algo al cliente*/
-let absolutePath = __dirname + '/views/index.html'
 
+app.use('/', (req, res, next)=>{
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();//Evita que el server se quede atorado.
+});
+
+let absolutePath = __dirname + '/views/index.html'
 app.get("/", (req, res)=>{
     res.sendFile(absolutePath);
 }); //Le diste un html con el método http get que obtuvo la ruta raíz y respondió con el index.
@@ -13,17 +15,13 @@ app.get("/", (req, res)=>{
 app.use('/public', express.static(__dirname + '/public'));//Montaste un middleware con .use, el middleware obtiene cosas estáticas.
 
 app.get('/json', (req, res)=>{
-    res.json({"message": "Hello json"});//Método de res que envía un json.
-}); //Enviaste información en un json.
-
-
-
-
-
-
-
-
-
+    process.env.MESSAGE_STYLE
+    if (process.env.MESSAGE_STYLE == 'uppercase') {
+        res.json({"message": "HELLO JSON"});
+    } else {
+        res.json({"message": "Hello json"});//Método de res que envía un json.
+    }
+}); 
 
 
 
