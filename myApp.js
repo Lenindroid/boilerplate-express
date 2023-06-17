07c22702a65cac5119178439b47ce683b2bf2dca
Lenen <leninrojassanchez09@gmail.com>
2023-06-16 21:16:31 -0500

    First commit

---
+++ myApp.js
@@ -0,0 +1,45 @@
+let express = require('express');
+let app = express();
+console.log('Hello World');
+/*app.get("/", (req, res)=>{
+    res.send("Hello Express");
+});*/
+let absolutePath = __dirname + '/views/index.html'
+
+app.get("/", (req, res)=>{
+    res.sendFile(absolutePath);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+
