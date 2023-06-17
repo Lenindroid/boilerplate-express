633d5d2d8f937e6207aa42f18b7bcdb6133a722c
Lenen <leninrojassanchez09@gmail.com>
2023-06-16 21:56:00 -0500

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -3,16 +3,15 @@ let app = express();
 console.log('Hello World');
 /*app.get("/", (req, res)=>{
     res.send("Hello Express");
-});*/
-let absolutePath = __dirname + '/views/index.html'
+}); RETO 1*/
+/*let absolutePath = __dirname + '/views/index.html'
 
 app.get("/", (req, res)=>{
     res.sendFile(absolutePath);
-});
+}); RETO 2*/
 
-
-
-
+let absolutePath = __dirname + '/public';
+app.use(express.static(absolutePath));
 
 
 
